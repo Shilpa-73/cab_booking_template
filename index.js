ab38e93a853d7827f5c01586971804321e3e40dd
shilpa-73 <shilpavasava73@gmail.com>
2021-05-11 18:53:49 +0530

    pre commit checking

--- index.js
+++ index.js
@@ -2,21 +2,17 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { graphqlHTTP } from 'express-graphql';
 import { GraphQLSchema } from 'graphql';
-const app = express()
-const port = 3000
 import { connect } from '@database';
 import rTracer from 'cls-rtracer';
 
 import { QueryRoot } from '@gql/queries';
 import { MutationRoot } from '@gql/mutations';
 import { logger } from '@utils/logger';
+const app = express();
+const port = 3000;
 
-const dkjkjkg = {}
-let unusedObj = {
-    a: 736473647,
+dotenv.config();
 
-
-}
 // connect to database
 connect();
 
@@ -26,21 +22,21 @@ const schema = new GraphQLSchema({ query: QueryRoot, mutation: MutationRoot });
 app.use(rTracer.expressMiddleware());
 
 app.use(
-    '/graphql',
-    graphqlHTTP({
-        schema: schema,
-        graphiql: true,
-        customFormatErrorFn: e => {
-            logger().info({ e });
-            return e;
-        }
-    })
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+    customFormatErrorFn: (e) => {
+      logger().info({ e });
+      return e;
+    }
+  })
 );
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello Welcome to the Cab-Booking API!')
-})
+  res.status(200).send('Hello Welcome to the Cab-Booking API!');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
