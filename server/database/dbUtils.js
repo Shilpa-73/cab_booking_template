86bf2a5aa97e85b0dc96e69b5fa360d99def8ae6
shilpa-73 <shilpavasava73@gmail.com>
2021-05-12 19:08:59 +0530

    camelCase fields added, built some daos for getting bookings and cabs

--- server/database/dbUtils.js
+++ server/database/dbUtils.js
@@ -115,6 +115,7 @@ export const transformDbArrayResponseToRawResponse = arr => {
 };
 
 
+
 export const mapKeysToCamelCase=arr=> arr.map(resource =>
     mapKeysDeep(removeDBReferenceKeyFromResponse(resource), keys => camelCase(keys))
 )
