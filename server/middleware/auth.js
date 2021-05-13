b3e44462783bb4bd8275041e465d910b0daaa55d
shilpa-73 <shilpavasava73@gmail.com>
2021-05-13 13:20:37 +0530

    testing

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -15,7 +15,6 @@ export const isAuthenticated = async (req, res, next) => {
     const jwtToken = req.headers.authorization || process.env.TEMP_TOKEN;
 
     const unused = {
-
     };
 
     // Todo to remove later static data above
